6bff63dedac8c36024b8f5943d9c2e9ff8864440
ndcahuang <B10209046@mail.ntust.edu.tw>
2018-03-15 11:59:57 +0800

    test result commit

---
+++ week_2/inclass.R
@@ -0,0 +1,23 @@
+require(XML)
+install.packages("XML")
+#data <- xmlTreeParse("http://feeds.bbci.co.uk/news/world/asia/rss.xml")
+# xmltop = xmlRoot(data)
+# xmlSize(xmltop)
+# gsub("<", "", xmltop[[1]][[10]][[1]][[1]])
+#xmltop[['channel']][['item']]
+# xmltop[['channel']][['item']]
+
+doc = xmlInternalTreeParse("http://feeds.bbci.co.uk/news/world/asia/rss.xml")
+doc
+items = getNodeSet(doc, "//channel/item")
+titles = lapply(items, xpathApply, ".//title", xmlValue)
+descriptions = lapply(items, xpathApply, ".//description", xmlValue)
+links = lapply(items, xpathApply, ".//link", xmlValue)
+dates = lapply(items, xpathApply, ".//pubDate", xmlValue)
+
+titles = unlist(titles)
+descriptions = unlist(descriptions)
+links = unlist(links)
+dates = unlist(dates)
+
+result = data.frame(Title=titles, Description=descriptions, Links=links, Dates=dates)
